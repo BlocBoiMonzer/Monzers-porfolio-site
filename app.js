8f7c9d3af52d6a0a99f1110e09887927602f6514
BlocBoiMonzer <77080735+BlocBoiMonzer@users.noreply.github.com>
2023-04-07 13:58:18 +0200

    Basic design down

---
+++ app.js
@@ -0,0 +1,7 @@
+// Get the button element
+const button = document.querySelector('button');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+  alert('You clicked the button!');
+});
